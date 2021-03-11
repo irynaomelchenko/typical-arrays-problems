3a0525ce4bed896fceb24e6f2f678f5c0803c770
Iryna Omelchenko <iren.omelchenko@gmail.com>
2021-03-11 10:46:39 +0200

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+
+  let minVal=1;
+
+  for (let i=0; i<array.length; i++) {
+    if (array[i]<minVal) {
+      minVal=array[i]
+    }
+  }
+  return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+
+  let maxVal=1;
+
+  for (let i=0; i<array.length; i++) {
+    if (array[i]>maxVal) {
+      maxVal=array[i]
+    }
+  }
+  return maxVal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let sum=0;
+
+  for (let i=0; i<array.length; i++) {
+    sum+=array[i]
+  }
+
+  let avgVal=sum/array.length
+  return avgVal;
 }
